bc4cdf226d45d212f48fff7bc3ad91eb27e9c0f2
yehie77447-cmd <yehie77447@gmail.com>
2026-09-14 17:44:24 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
 
 export default defineConfig({
-  base: './', // السطر المسؤول عن حل مشكلة الشاشة السوداء في أندرويد
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     react(),
     VitePWA({
